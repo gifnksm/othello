204524dbcb574c58f529f62d8a89792e2ca784c0
gifnksm <makoto.nksm+github@gmail.com>
2016-01-02 22:06:07 +0900

    App splits into ViewConfig

--- src/main.rs
+++ src/main.rs
@@ -56,7 +56,11 @@ impl Side {
 #[derive(Clone, Debug)]
 struct App {
     board: Board,
+    view_config: ViewConfig,
+}
 
+#[derive(Copy, Clone, Debug)]
+struct ViewConfig {
     frame_width: f64,
     cell_size: f64,
     disk_radius: f64,
@@ -70,11 +74,9 @@ struct App {
     black_color: Color,
 }
 
-impl Default for App {
-    fn default() -> App {
-        App {
-            board: Board::new(Size(8, 8)),
-
+impl Default for ViewConfig {
+    fn default() -> ViewConfig {
+        ViewConfig {
             frame_width: 1.0,
             cell_size: 80.0,
             disk_radius: 32.0,
@@ -86,6 +88,15 @@ impl Default for App {
             board_color: color::rgba(0.0, 0.5, 0.0, 1.0),
             white_color: color::white(),
             black_color: color::black(),
+        }
+    }
+}
+
+impl Default for App {
+    fn default() -> App {
+        App {
+            board: Board::new(Size(8, 8)),
+            view_config: ViewConfig::default(),
         }
     }
 }
@@ -106,9 +117,9 @@ fn main() {
                          .for_folder("assets")
                          .unwrap();
         let ref font_path = assets.join("FiraSans-Regular.ttf");
-        let theme = Theme::default();
         let factory = window.factory.borrow().clone();
         let glyph_cache = Glyphs::new(font_path, factory).unwrap();
+        let theme = Theme::default();
         Ui::new(glyph_cache, theme)
     };
 
@@ -130,95 +141,87 @@ widget_ids! {
 }
 
 fn set_widgets(ui: &mut Ui, app_ref: Rc<RefCell<App>>) {
-    let Size(rows, cols) = {
+    let (Size(rows, cols), vc) = {
         let app = app_ref.deref().borrow();
-        Canvas::new().color(app.board_color).scrolling(true).set(CANVAS, ui);
-        app.board.size()
+        let size = app.board.size();
+        (size, app.view_config)
     };
 
-    {
-        let app = app_ref.deref().borrow();
-        let board_width = app.cell_size * (app.board.size().1 as f64);
-        let indicator_width = app.cell_size + app.indicator_text_width;
-        let width = board_width + app.board_margin * 2.0 + indicator_width + app.board_margin;
+    Canvas::new().color(vc.board_color).scrolling(true).set(CANVAS, ui);
 
-        let board_height = app.cell_size * (app.board.size().0 as f64);
-        let indicator_height = app.cell_size * 2.0;
-        let height = app.board_margin * 2.0 + f64::max(board_height, indicator_height);
+    let board_width = vc.cell_size * (cols as f64);
+    let indicator_width = vc.cell_size + vc.indicator_text_width;
+    let width = board_width + vc.board_margin * 2.0 + indicator_width + vc.board_margin;
 
-        let style = LineStyle::new().thickness(0.0);
-        let rect = Rectangle::outline_styled([width, height], style);
+    let board_height = vc.cell_size * (rows as f64);
+    let indicator_height = vc.cell_size * 2.0;
+    let height = vc.board_margin * 2.0 + f64::max(board_height, indicator_height);
 
-        match (ui.win_w < board_width, ui.win_h < board_height) {
-            (true, true) => rect.top_left_of(CANVAS),
-            (false, true) => rect.mid_top_of(CANVAS),
-            (true, false) => rect.mid_left_of(CANVAS),
-            (false, false) => rect.middle_of(CANVAS),
-        }
-        .set(OTHELLO_CANVAS, ui);
+    let style = LineStyle::new().thickness(0.0);
+    let rect = Rectangle::outline_styled([width, height], style);
+
+    match (ui.win_w < board_width, ui.win_h < board_height) {
+        (true, true) => rect.top_left_of(CANVAS),
+        (false, true) => rect.mid_top_of(CANVAS),
+        (true, false) => rect.mid_left_of(CANVAS),
+        (false, false) => rect.middle_of(CANVAS),
     }
+    .set(OTHELLO_CANVAS, ui);
 
-    let matrix = {
-        let app = app_ref.deref().borrow();
-        WidgetMatrix::new(cols as usize, rows as usize)
-            .top_left_with_margins_on(OTHELLO_CANVAS, app.board_margin, app.board_margin)
-            .w_h(app.cell_size * (cols as f64), app.cell_size * (rows as f64))
-    };
+    WidgetMatrix::new(cols as usize, rows as usize)
+        .top_left_with_margins_on(OTHELLO_CANVAS, vc.board_margin, vc.board_margin)
+        .w_h(vc.cell_size * (cols as f64), vc.cell_size * (rows as f64))
+        .each_widget(|_n, col, row| {
+            let pt = Point(row as i32, col as i32);
+            let disk = {
+                let app_ref = app_ref.clone();
+                let app = app_ref.deref().borrow();
+                let mut disk = OthelloDisk::new()
+                                   .background_color(vc.board_color)
+                                   .frame(vc.frame_width)
+                                   .frame_color(vc.frame_color)
+                                   .white_color(vc.white_color)
+                                   .black_color(vc.black_color)
+                                   .radius(vc.disk_radius)
+                                   .disk(app.board[pt]);
+                if let Some(turn) = app.board.turn() {
+                    if app.board.can_locate(pt) {
+                        disk = disk.flow_disk(Some(turn));
+                    }
+                }
+                disk
+            };
 
-    matrix.each_widget(|_n, col, row| {
-              let pt = Point(row as i32, col as i32);
-              let disk = {
-                  let app_ref = app_ref.clone();
-                  let app = app_ref.deref().borrow();
-                  let mut disk = OthelloDisk::new()
-                                     .background_color(app.board_color)
-                                     .frame(app.frame_width)
-                                     .frame_color(app.frame_color)
-                                     .white_color(app.white_color)
-                                     .black_color(app.black_color)
-                                     .radius(app.disk_radius)
-                                     .disk(app.board[pt]);
-                  if let Some(turn) = app.board.turn() {
-                      if app.board.can_locate(pt) {
-                          disk = disk.flow_disk(Some(turn));
-                      }
-                  }
-                  disk
-              };
+            let app_ref = app_ref.clone();
+            disk.react(move || {
+                app_ref.borrow_mut().board.locate(pt);
+            })
+        })
+        .set(BOARD, ui);
 
-              let app_ref = app_ref.clone();
-              disk.react(move || {
-                  app_ref.borrow_mut().board.locate(pt);
-              })
-          })
-          .set(BOARD, ui);
-
-    {
-        let app = app_ref.deref().borrow();
-        let x = app.cell_size * ((cols / 4) as f64);
-        let y = app.cell_size * ((rows / 4) as f64);
-        let signs = &[(-1.0, 1.0), (1.0, 1.0), (-1.0, -1.0), (1.0, -1.0)];
-        for (i, &(sx, sy)) in signs.iter().enumerate() {
-            Circle::fill(app.dot_radius)
-                .x_y_relative_to(BOARD, sx * x, sy * y)
-                .color(app.frame_color)
-                .set(DOT + i, ui);
-        }
+    let x = vc.cell_size * ((cols / 4) as f64);
+    let y = vc.cell_size * ((rows / 4) as f64);
+    let signs = &[(-1.0, 1.0), (1.0, 1.0), (-1.0, -1.0), (1.0, -1.0)];
+    for (i, &(sx, sy)) in signs.iter().enumerate() {
+        Circle::fill(vc.dot_radius)
+            .x_y_relative_to(BOARD, sx * x, sy * y)
+            .color(vc.frame_color)
+            .set(DOT + i, ui);
     }
 
     for (i, &side) in [Side::Black, Side::White].iter().enumerate() {
         let app = app_ref.deref().borrow();
         if side == Side::Black {
-            OthelloDisk::new().right_from(BOARD, app.board_margin)
+            OthelloDisk::new().right_from(BOARD, vc.board_margin)
         } else {
             OthelloDisk::new().down_from(INDICATOR_LABEL_ICON + (i - 1), 0.0)
         }
-        .w_h(app.cell_size, app.cell_size)
-        .background_color(app.board_color)
+        .w_h(vc.cell_size, vc.cell_size)
+        .background_color(vc.board_color)
         .frame(0.0)
-        .white_color(app.white_color)
-        .black_color(app.black_color)
-        .radius(app.disk_radius)
+        .white_color(vc.white_color)
+        .black_color(vc.black_color)
+        .radius(vc.disk_radius)
         .disk(Some(side))
         .react(|| {})
         .set(INDICATOR_LABEL_ICON + i, ui);
@@ -228,7 +231,7 @@ fn set_widgets(ui: &mut Ui, app_ref: Rc<RefCell<App>>) {
             Side::White => app.board.num_white().to_string(),
         };
         Text::new(&text)
-            .w(app.indicator_text_width)
+            .w(vc.indicator_text_width)
             .right_from(INDICATOR_LABEL_ICON + i, 0.0)
             .font_size(60)
             .align_text_right()
